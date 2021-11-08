2dfde9ecc0c8c3f89d1ef0cc8c899f9e8dd72156
TayteStokes <taytestokes9328@gmail.com>
2021-11-08 16:44:11 -0700

    Configures axios instance with data needed to access the PodiumApi

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,13 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+
+// import { render, screen } from '@testing-library/react';
+// import App from './App';
+
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
