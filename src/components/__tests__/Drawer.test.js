267c47f67dc05e9510b4c456bdd7f28023b07b17
TayteStokes <taytestokes9328@gmail.com>
2021-11-12 17:55:33 -0700

    fixes test imports

--- src/components/__tests__/Drawer.test.js
+++ src/components/__tests__/Drawer.test.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 
-import { Drawer } from './Drawer'
+import { Drawer } from '../Drawer'
 
 const defaultProps = {
   onClose: jest.fn(),
